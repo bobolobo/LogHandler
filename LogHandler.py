8b83b44a9539bbfdd53e5dbdd28617fbfcfc175e
bobolobo <bferrari@comcast.net>
2020-02-11 11:26:18 -0500

    Added BioFDRS directory for log collection.

--- LogHandler.py
+++ LogHandler.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+from os import listdir
+import re
 import glob
 import shutil
 
@@ -22,15 +24,31 @@ def command_line_arguments():
         print(err)
         return
 
+def clear_destination():
+    """ In the destination drive/folder, remove all existing .log files """
+
+    folder_path = r'd:/TEMP/'
+
+    print("Removing existing files in destination: ")
+
+    for file_name in listdir(folder_path):
+        if re.search(".log*", file_name):
+            real_path = os.path.join(folder_path, file_name)
+            os.remove(real_path)
+            print(real_path)
+
+    return
+
 
 def cleaner():
     """ Remove log files """
 
     # Get recursive lists of file paths that matches pattern including sub directories
-    file_list1 = glob.glob('/Program Files/IDEMIA/*.log*', recursive=True)
-    file_list2 = glob.glob('/STIP/*.log*', recursive=False)
-    file_list = file_list1 + file_list2
-    print(file_list)
+    file_list1 = glob.glob(r'/Program Files/IDEMIA/*.log*', recursive=True)
+    file_list2 = glob.glob(r'/STIP/*.log*', recursive=False)
+    file_list3 = glob.glob(r'/ECAT/BioFDRS/*.log*', recursive=False)
+    file_list = file_list1 + file_list2 + file_list3
+    #print("FILE LIST: ", file_list)
 
     # Iterate over the list of filepaths & remove each file.
     for filePath in file_list:
@@ -45,14 +63,16 @@ def cleaner():
 def download():
     """ Download log files to D: USB stick """
 
-    dest = r'd:\temp'
+    dest = r'd:/temp'
 
-    file_list1 = glob.glob('/Program Files/IDEMIA/*.log*', recursive=True)
-    file_list2 = glob.glob('/STIP/*.log*', recursive=False)
+    clear_destination()  # Remove existing .log \files in the destination
 
-    file_list = file_list1 + file_list2
+    file_list1 = glob.glob(r'/Program Files/IDEMIA/*.log*', recursive=True)
+    file_list2 = glob.glob(r'/STIP/*.log*', recursive=False)
+    file_list3 = glob.glob(r'/ECAT/BioFDRS/*.log*', recursive=False)
+    file_list = file_list1 + file_list2 + file_list3
 
-    print("Downloading files:", )
+    print("\nDownloading files:", )
     for file_name in file_list:
         shutil.copy(file_name, dest)
         print(file_name)
@@ -62,7 +82,6 @@ def download():
 def main():
 
     choice = command_line_arguments()
-    print(choice.action)
 
     if choice.action == "clean":
         cleaner()
